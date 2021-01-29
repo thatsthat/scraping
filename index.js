ff609dbf8c0f7ed9815790bf9607fb5acc395209
Aram Anto <aram.anto@gmail.com>
2021-01-29 11:39:34 +0100

    save to file

--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 const cheerio = require("cheerio");
 const fs = require("fs");
-const { contains } = require("cheerio");
+const parse2csv = require("json2csv");
 
 puppeteer.use(StealthPlugin());
 
 async function run() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
-
+  // loop over result pages
+  var cars = [];
   for (j = 1; j < 5; j++) {
     let url = `https://www.coches.net/hibrido/segunda-mano/barcelona/?pg=${j}&MaxKms=100000&KeyWords=auris&Fueltype2`;
     await page.goto(url);
     let content = await page.content();
     var $ = cheerio.load(content);
+    // loop over all cars in the page
     $(".mt-CardAd-link").each(function (i, element) {
       var a = $(this);
       var model = a.children().eq(0).text().trim();
@@ -31,9 +33,25 @@ async function run() {
         .replace(" km", "")
         .replace(".", "");
       var year = a.find("li:contains('km')").prev().text().trim();
-      console.log(kms);
+      var car = {
+        km: kms,
+        price: precio,
+        any: year,
+        car: model,
+      };
+      cars.push(car);
     });
   }
   browser.close();
+  // Save results to csv file
+  const resFields = Object.keys(cars[0]);
+  const opts = { fields: resFields, withBOM: true };
+  const resul_csv = parse2csv.parse(cars, opts);
+  fs.writeFile("cars.csv", resul_csv, (err) => {
+    if (err) throw err;
+    console.log("Cars saved!");
+  });
+
+  //console.log(cars);
 }
 run();
